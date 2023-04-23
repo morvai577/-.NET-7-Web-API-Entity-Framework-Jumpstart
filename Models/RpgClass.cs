a12a6444df3d1c663a22919077e40322f74d9956
morvai577 <thevaimor@icloud.com>
2023-04-23 17:48:58 +1200

    Update RpgClass.cs

--- Models/RpgClass.cs
+++ Models/RpgClass.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace dotnet_rpg.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))] // 1. The JsonStringEnumConverter attribute is used to convert an enum to a string when serializing and deserializing JSON.
     public enum RpgClass
     {
         Knight = 1,
