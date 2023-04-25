727267f0ad4391094e7719e5ebff217098038262
Vaibhav Mor <thevaimor@icloud.com>
2023-04-25 18:10:01 +1200

    Add DTOs

---
+++ DTOs/Character/GetCharacterResDto.cs
@@ -0,0 +1,13 @@
+namespace dotnet_rpg.DTOs.Character
+{
+    public class GetCharacterResDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo"; // Default value
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
