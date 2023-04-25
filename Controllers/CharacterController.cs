f70c8bd9ca3a1fd995276bd4e7dd22c7742d16cb
Vaibhav Mor <thevaimor@icloud.com>
2023-04-25 18:32:26 +1200

    Add new endpoint for PUT character
    - Returns 200 for updated character
    - Returns 404 for character not found

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -35,5 +35,18 @@ namespace dotnet_rpg.Controllers
         {
             return Ok(await _characterService.AddCharacter(newCharacter));
         }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterResDto>>>> UpdateCharacter(UpdateCharacterReqDto updatedCharacter)
+        {
+            var response = await _characterService.UpdateCharacter(updatedCharacter);
+
+            if (response.Data is null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
     }
 }
